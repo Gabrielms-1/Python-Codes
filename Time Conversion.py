005f3dcbedaf898f64ba3cac69e429c2bcd5b33e
Gabrielms-1 <40607345+Gabrielms-1@users.noreply.github.com>
2021-05-09 01:40:27 -0300

    PEP-8 rules commitment

--- Time Conversion.py
+++ Time Conversion.py
@@ -1,11 +1,9 @@
-# Conversão do Tempo #1019
-
-# Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, e informe-o expresso no formato horas:minutos:segundos.
-
 tempo = int(input())
 segundo = tempo % 60
 minutos = tempo / 60
 minuto = minutos % 60
 hora = minutos / 60
 
-print('{0:.0f}'.format(hora),':','{0:.0f}'.format(minuto),':','{0:.0f}'.format(segundo))v
+print('{0:.0f}'.format(hora),
+      ':', '{0:.0f}'.format(minuto), ':',
+      '{0:.0f}'.format(segundo))
