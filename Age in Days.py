b5f6b012c993759aa7945c50616460739b18421b
Gabrielms-1 <40607345+Gabrielms-1@users.noreply.github.com>
2021-05-09 01:34:01 -0300

    PEP-8 rules commitment

--- Age in Days.py
+++ Age in Days.py
@@ -1,8 +1,8 @@
-dias= int(input())
-ano= dias//365
+dias = int(input())
+ano = dias//365
 print(ano, 'ano(s)')
-meses= dias%365
-mes= meses//30
+meses = dias % 365
+mes = meses//30
 print(mes, 'mes(es)')
-dia= meses%30
+dia = meses % 30
 print(dia, 'dia(s)')
